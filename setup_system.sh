a08a2b468e7cdf020625215ecd06db58bdd86aba
Anonim-khb <alvo4444@gmail.com>
2017-01-10 23:52:23 +1000

    1

--- setup_system.sh
+++ setup_system.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# User setup
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 
 # Variables
 DBHOST=localhost
